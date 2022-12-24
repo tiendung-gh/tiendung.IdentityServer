a45436516eaa9b3c56cbaae03c17e0d15f2242a2
tiendung <tiendung.gg9@gmail.com>
2022-12-24 21:50:15 +0700

    add UI and basic Config

--- tiendung.IdentityServer.WebApp/Program.cs
+++ tiendung.IdentityServer.WebApp/Program.cs
@@ -1,6 +1,30 @@
+using tiendung.IdentityServer.WebApp.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+                .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+                .AddTestUsers(InMemoryConfig.GetUsers())
+                .AddInMemoryClients(InMemoryConfig.GetClients())
+                .AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
